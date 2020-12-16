71f5d35dfcf51d850b3476fb1f628d304288f74e
ozone4real <ezenwaogbonna1@gmail.com>
2020-12-16 03:12:19 +0100

    fix up analytics

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,12 +1,11 @@
 ActiveSupport::Notifications.subscribe "perform.active_job" do |_, started, ended, id, payload|
   job = payload[:job]
-  if job.executions <= 1
+  if job.respond_to?(:exception)
+    # send/update failure metrics
+  else
     duration = ended - started
-    puts "Took #{duration}s"
     # send/update jobs count/duration metrics with a payload like:
     # { name: "job_run", job: payload , duration: duration }
-    if job.respond_to? :exception
-      # send/update failure metrics
-    end
   end
 end
+
